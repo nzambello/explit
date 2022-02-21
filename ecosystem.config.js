ea114a4418b0b4c3d48c0ebbbce82e13e659268f
nzambello <nzambello@protonmail.com>
2022-02-21 16:08:47 +0100

    ci: fix port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,9 +9,11 @@ module.exports = {
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "production",
+        PORT: 5001,
       },
       env_production: {
         NODE_ENV: "production",
+        PORT: 5001,
       },
     },
   ],
